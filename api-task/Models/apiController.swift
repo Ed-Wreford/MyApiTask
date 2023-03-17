e7fa962e119efaeeb3781323d096d23412fb9ec8
Leo Wildman <wildman.l@etoncollege.org.uk>
2023-03-17 16:45:30 +0000

    add stuff

--- api-task/Models/apiController.swift
+++ api-task/Models/apiController.swift
@@ -7,8 +7,41 @@
 
 import Foundation
 
+struct KanyeResponse:Codable {
+    var quote: String
+}
 
 //To DO
-func getData() -> String{
-    return ""
+
+
+class StateController: ObservableObject {
+    
+    func getData() -> String {
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { (data, respomse, error) in
+            if let data = data {
+                if let response = self.parseJson(json: data) {
+                    let names = response.results.map {
+                        return $0.name
+                    }
+                    DispatchQueue.main.async {
+                        self.artistsByLocation = names.joined(separator: ", ")
+                    }
+                }
+            }
+        }.resume()
+    }
+
+    func parseJson(json: Data) -> KanyeResponse? {
+        let decoder = JSONDecoder()
+        
+        if let kanyeResponse = try? decoder.decode(KanyeResponse.self, from: json) {
+            return kanyeResponse
+        } else {
+            print("Error decoding JSON")
+            return nil
+        }
+    }
+    
 }
