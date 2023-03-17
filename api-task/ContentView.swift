77180dda08a31fe8dc0cd739204a47dcc87b4b14
Leo Wildman <wildman.l@etoncollege.org.uk>
2023-03-17 15:21:36 +0000

    Build basic UI, groundwork

--- api-task/ContentView.swift
+++ api-task/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var output = ""
+    
+    func go(){
+        output = getData()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            Text("Title")
+            TextEditor(text: $output)
+                .disabled(true)
+            Spacer()
+            Button(action: {
+                go()
+            }) {
+                HStack {
+                    Image(systemName: "highlighter")
+                    Text("Go!")
+                }
+                .foregroundColor(.white)
+                .background(LinearGradient(gradient: Gradient(colors: [.pink, .purple]), startPoint: .leading, endPoint: .trailing))
+                .cornerRadius(40)
+                .frame(maxWidth: .infinity)
+                .padding()
+            }
             .padding()
+        }
     }
 }
 
---
+++ api-task/Models/apiController.swift
@@ -0,0 +1,14 @@
+//
+//  apiController.swift
+//  api-task
+//
+//  Created by Wildman, Leo (RCH) on 17/03/2023.
+//
+
+import Foundation
+
+
+//To DO
+func getData() -> String{
+    return ""
+}
