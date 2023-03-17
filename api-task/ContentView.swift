a65e8e482fce2ad712c87850ecf82df87a598b9e
Leo Wildman <wildman.l@etoncollege.org.uk>
2023-03-17 14:46:26 +0000

    Initial Commit

---
+++ api-task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  api-task
+//
+//  Created by Wildman, Leo (RCH) on 17/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ api-task/api_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  api_taskApp.swift
+//  api-task
+//
+//  Created by Wildman, Leo (RCH) on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct api_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
